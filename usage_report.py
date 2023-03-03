5f2814d6c675d1e122ce75738ac9e36cb557c26a
Jonathan Klick <jklick@stackoverflow.com>
2023-03-03 15:08:29 -0500

    added a bunch of CSV reporting

--- usage_report.py
+++ usage_report.py
@@ -31,11 +31,12 @@ def get_items(endpoint, filter_id=''):
         endpoint (string): the API endpoint containing the data to read. No leading "/" is required
             since this is already taken care of in the BASE_API_URL variable
         filter_id (string): this is an optional filter parameter that can be added to the API call.
-            By default, this Python script uses filters to strip out any PII or CI data.
+            By default, this Python script uses filters to strip out any PII or CI data, as well as
+            include some useful fields that are hidden by default.
             For more details on filters, see https://api.stackexchange.com/docs/filters
 
     Returns:
-        items (list): this is a list of dictionaries containing the requested data
+        items (list): a list of dictionaries, each containing an object of the requested type
     """
 
     # Establish API call parameters
@@ -97,18 +98,18 @@ def create_usage_report():
     answers = read_json(ANSWERS_ENDPOINT+".json")
 
     # create lists of anonymized users based on different criteria
-    tag_contributors, question_contributors, answer_contributors = create_contributor_lists(
-        questions, answers)
+    tag_contributors, question_contributors, answer_contributors, comment_contributors = \
+        create_contributor_lists(questions, answers)
     asked_and_answered = [value for value in question_contributors if value in answer_contributors]
-    all_contributors = question_contributors + answer_contributors
-    deduped_contributors = dict.fromkeys(all_contributors)
+    all_unique_contributors = set(
+        question_contributors + answer_contributors + comment_contributors)
 
     # count the amount of users in each list
     user_count = len(users)
     asker_count = len(question_contributors)
     answerer_count = len(answer_contributors)
     asked_and_answered_count = len(asked_and_answered)
-    contributor_count = len(deduped_contributors)
+    contributor_count = len(all_unique_contributors)
     users_with_badges = calculate_user_badges(users)
 
     # calculate post and tag metrics
@@ -134,7 +135,6 @@ def create_usage_report():
     print(f"Total questions: {question_count}")
     print(f"Total answers: {answer_count}")
     print(f"Total page views across questions: {total_view_count}")
-    print()
 
     create_tag_report(questions, answers, tag_contributors)
 
@@ -167,34 +167,57 @@ def create_contributor_lists(questions, answers):
     tag_contributors = []
     question_contributors = []
     answer_contributors = []
+    comment_contributors = []
 
     for question in questions:
-        user_id = validate_user_id(question)
-        question_contributors = add_user_to_list(user_id, question_contributors)
+        asker_id = validate_user_id(question)
+        question_contributors = add_user_to_list(asker_id, question_contributors)
         for tag in question['tags']:
-            tag_index = find_dict_in_list(tag, 'tag', tag_contributors)
-            if tag_index is None:
-                tag_contributors.append(
-                    {
-                        'tag': tag,
-                        'askers': [user_id],
-                        'answerers': []
-                    }
-                )
-            else:
-                tag_contributors[tag_index]['askers'] = add_user_to_list(
-                user_id, tag_contributors[tag_index]['askers'])
+            while True:
+                tag_index = find_dict_in_list(tag, 'tag', tag_contributors)
+                if tag_index is None:
+                    tag_contributors.append(
+                        {
+                            'tag': tag,
+                            'askers': [],
+                            'answerers': [],
+                            'commenters': []
+                        }
+                    )
+                else:
+                    break
+
+            tag_contributors[tag_index]['askers'] = add_user_to_list(
+                asker_id, tag_contributors[tag_index]['askers']
+            )
+
+            if question.get('comments'):
+                for comment in question['comments']:
+                    commenter_id = validate_user_id(comment)
+                    comment_contributors = add_user_to_list(commenter_id, comment_contributors)
+                    tag_contributors[tag_index]['commenters'] = add_user_to_list(
+                        commenter_id, tag_contributors[tag_index]['commenters']
+                    )
 
     for answer in answers:
-        user_id = validate_user_id(question)
-        answer_contributors = add_user_to_list(user_id, answer_contributors)
+        answerer_id = validate_user_id(answer)
+        answer_contributors = add_user_to_list(answerer_id, answer_contributors)
         question = find_dict_in_list(answer['question_id'], 'question_id', questions, False)
+
         for tag in question['tags']:
             tag_index = find_dict_in_list(tag, 'tag', tag_contributors)
             tag_contributors[tag_index]['answerers'] = add_user_to_list(
-                user_id, tag_contributors[tag_index]['answerers'])
+                answerer_id, tag_contributors[tag_index]['answerers'])
 
-    return tag_contributors, question_contributors, answer_contributors
+            if answer.get('comments'):
+                for comment in answer['comments']:
+                    commenter_id = validate_user_id(comment)
+                    comment_contributors = add_user_to_list(commenter_id, comment_contributors)
+                    tag_contributors[tag_index]['commenters'] = add_user_to_list(
+                        commenter_id, tag_contributors[tag_index]['commenters']
+                    )
+
+    return tag_contributors, question_contributors, answer_contributors, comment_contributors
 
 
 def create_tag_report(questions, answers, tag_contributors):
@@ -204,44 +227,72 @@ def create_tag_report(questions, answers, tag_contributors):
         for tag in question['tags']:
             try:
                 tag_metrics[tag]['question_count'] += 1
-                tag_metrics[tag]['view_count'] += question['view_count']
-            except KeyError:
+            except KeyError: # if tag doesn't have a dictionary created yet, initialize one
                 tag_metrics[tag] = {
+                    'view_count': 0,
+                    'unique_askers': 0,
+                    'unique_answerers': 0,
+                    'unique_commenters': 0,
+                    'unique_contributors': 0,
                     'question_count': 1,
-                    'view_count': question['view_count'],
-                    'answer_count': 0
+                    'question_upvotes': 0,
+                    'question_downvotes': 0,
+                    'question_comments': 0,
+                    'questions_no_answers': 0,
+                    'questions_accepted_answer': 0,
+                    'answer_count': 0,
+                    'answer_upvotes': 0,
+                    'answer_downvotes': 0,
+                    'answer_comments': 0,
                 }
+            tag_metrics[tag]['view_count'] += question['view_count']
+            tag_metrics[tag]['question_upvotes'] += question['up_vote_count']
+            tag_metrics[tag]['question_downvotes'] += question['down_vote_count']
+
+            try:
+                tag_metrics[tag]['question_comments'] += len(question['comments'])
+            except KeyError: # if there are no comments, a KeyError will be produced
+                pass
+
+            if question['answer_count'] == 0:
+                tag_metrics[tag]['questions_no_answers'] += 1
+            if question['is_answered'] == True:
+                tag_metrics[tag]['questions_accepted_answer'] += 1
+
+            tag_contributor_data = find_dict_in_list(tag, 'tag', tag_contributors, False)
+            tag_metrics[tag]['unique_askers'] = len(tag_contributor_data['askers'])
+            tag_metrics[tag]['unique_answerers'] = len(tag_contributor_data['answerers'])
+            tag_metrics[tag]['unique_commenters'] = len(tag_contributor_data['commenters'])
+            tag_metrics[tag]['unique_contributors'] = len(set(
+                tag_contributor_data['askers'] + tag_contributor_data['answerers'] +
+                tag_contributor_data['commenters']
+                ))
 
     for answer in answers:
         question = find_dict_in_list(answer['question_id'], 'question_id', questions, False)
         for tag in question['tags']:
             tag_metrics[tag]['answer_count'] += 1
+            tag_metrics[tag]['answer_upvotes'] += answer['up_vote_count']
+            tag_metrics[tag]['answer_downvotes'] += answer['down_vote_count']
+            try:
+                tag_metrics[tag]['answer_comments'] += len(answer['comments'])
+            except KeyError: # if there are no comments, a KeyError will be produced
+                pass
 
     tags_sorted_by_view_count = sorted(
         tag_metrics.items(), key = lambda x: x[1]['view_count'], reverse=True)
 
-    print("TOP TAGS")
     csv_data = []
-    for i, tag_data in enumerate(tags_sorted_by_view_count):
+    for tag_data in tags_sorted_by_view_count:
         tag = tag_data[0]
-        view_count = tag_data[1]['view_count']
-        question_count = tag_data[1]['question_count']
-        answer_count = tag_data[1]['answer_count']
-
-        contributor_data = find_dict_in_list(tag, 'tag',tag_contributors, False)
-        askers_count = len(contributor_data['askers'])
-        answerers_count = len(contributor_data['answerers'])
-        csv_row = [tag, view_count, question_count, answer_count, askers_count, answerers_count]
+        csv_row = [tag] + list(tag_data[1].values())
         csv_data.append(csv_row)
 
-        if i < 20:
-            output_text = f"{tag} | page views: {view_count} | questions: {question_count} | " \
-                f"answers: {answer_count} | unique askers: {askers_count} | " \
-                f"unique answerers: {answerers_count}"
-            print(output_text)
-
-    csv_header = ['tag', 'page views', 'question count', 'answer count', 'unique askers', \
-            'unique answerers']
+    csv_header = ['Tag', 'Aggregate Page Views', 'Unique Askers', 'Unique Answerers',
+                  'Unique Commenters', 'Unique Contributors','Question Count', 'Question Upvotes',
+                  'Question Downvotes', 'Question Comments', 'Questions Without Answers',
+                  'Questions With Accepted Answers', 'Answer Count', 'Answer Upvotes',
+                  'Answer Downvotes', 'Answer Comments']
     csv_file_name = 'tag_metrics.csv'
     write_csv(csv_file_name, csv_header, csv_data)
 
@@ -251,11 +302,11 @@ def add_user_to_list(user_id, user_list):
     the list.
 
     Args:
-        user_id (int): the ID of the user in Stack Overflow
+        user_id (int): the unique user ID of a particular user in Stack Overflow
         user_list (list): current user list
 
     Returns:
-        list: updated user list
+        user_list (list): updated user list
     """
     if user_id not in user_list:
         user_list.append(user_id)
@@ -273,8 +324,8 @@ def find_dict_in_list(id, key, list_of_dicts, return_index=True):
         id (string): the dictionary value that is being searched for
         key (string): the dictionary key that will contain the value searched for
         list_of_dicts (list): the list of dictionaries being searched through
-        return_index (bool, optional): When True, the function returns the list index of the 
-            dictionary. When False, the function returns the contents of the dictionary.
+        return_index (bool, optional): When True, the function returns the list index of the
+            dictionary. When False, the function returns the whole dictionary.
             Defaults to True.
 
     Returns:
@@ -300,10 +351,21 @@ def calculate_user_badges(users):
 
 
 def validate_user_id(item):
+    """This function looks at an item (e.g. question, answer, or comment) and validates if the user
+    still exists. For further context, when a user is deleted from the platform, their content will
+    remain behind, but it will no longer be attributed to anyone in particular. If a user_id does
+    not exist for the user, a user_id of "unknown" will be assigned instead.
 
+    Args:
+        item (dict): the dictionary for a question, answer, or comment
+
+    Returns:
+        user_id (string): the user ID to whom the content is attributed, or "unknown" if the user
+            no longer exists
+    """
     try:
-        user_id = item['owner']['account_id']
-    except KeyError: # If an owner account has been deleted, user_id will not exist
+        user_id = item['owner']['user_id']
+    except KeyError:
         user_id = 'unknown'
 
     return user_id
@@ -323,7 +385,7 @@ def write_csv(file_name, header, data):
 
 
 if __name__ == '__main__':
-    get_items(QUESTIONS_ENDPOINT, "!)QCGvppz4*yNaovd_i*d5VRH")
-    get_items(ANSWERS_ENDPOINT, "!)Q5ZKXZC3gAkrZ83hw9t3b47")
-    get_items(USERS_ENDPOINT, "!0Z-LvhH.I(HSQL-9.EeSLuce2")
+    get_items(QUESTIONS_ENDPOINT, "!t-MolHjKtPLQXQGxGwuxRiz2FAIbzBW")
+    get_items(ANSWERS_ENDPOINT, "!1zhxWdTMzIx6vivJYIYoB")
+    get_items(USERS_ENDPOINT, "!LnO)*TtSsJGGD5.dHo0NYN")
     create_usage_report()
